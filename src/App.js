5c25e8695498c568a1dea596b365f656b2d0e338
Thurain21 <thurainkosg@gmail.com>
2024-09-22 17:21:59 +0800

    add undo delete function

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
-
-import { useState } from 'react';
-import './App.css';
-import Button from './components/Button';
-import Input from './components/Input';
-import Output from './components/Output';
+import { useState } from "react";
+import "./App.css";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import Output from "./components/Output";
 
 function App() {
+  const [output, setOutput] = useState(0);
+  const [input, setInput] = useState(0);
 
-  const [output,setOutput] = useState(0);
-  const [input,setInput]=useState(0);
-
-  function clickEventHandler(value){
-
-    if(value=='C'){
+  function clickEventHandler(value) {
+    if (value == "C") {
       setInput(0);
       setOutput(0);
-    }else if(input == 0){
+
+    }else if (value == "Dele") {
+
+      if(input!= 0){
+        input.length==1? setInput(0) : setInput(input.slice(0,input.length-1));
+      }
+    } else if (input == 0) {
       setInput(value);
-    }
-    else if(value =='='){
+    } else if (value == "=") {
       setOutput(eval(input));
       setInput(0);
+    }  else {
+      setInput(input + value);
     }
-    else{
-      setInput(input+value);
-    }
-   
-    
-    
-    
   }
 
   return (
-      <div className='container'>
-        <div className='frame'>
-          <Output output={output}/>
-         <Input input={input} />
-        <Button clickeventhandler={clickEventHandler}/>
-        </div>
-        
-      
+    <div className="container">
+      <div className="frame">
+        <Output output={output} />
+        <Input input={input} />
+        <Button clickeventhandler={clickEventHandler} />
+      </div>
     </div>
-
-    
   );
 }
 
